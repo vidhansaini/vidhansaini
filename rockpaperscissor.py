a3a2af80642e54480432b01af3ad4a7a4cffe2f2
vidhansaini <139987246+vidhansaini@users.noreply.github.com>
2024-08-19 22:27:23 +0530

    Update rockpaperscissor.py

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -1,44 +1,46 @@
 import random
 
-rock = "0"
-paper = "1"
-scissor = "2"
-print(rock," is rock")
-print(paper," is paper")
-print(scissor," is scissor")
-player = input("ENTER YOUR CHOICE\n")
+def rps():
+    rock = "0"
+    paper = "1"
+    scissor = "2"
+    print(rock," is rock")
+    print(paper," is paper")
+    print(scissor," is scissor")
+    player = input("ENTER YOUR CHOICE\n")
+    
+    if player == rock:
+        print(f"YOUR CHOICE IS ROCK")
+    elif player == paper:
+        print(f"YOUR CHOICE IS PAPER")
+    elif player == scissor:
+        print(f"YOUR CHOICE IS SCISSOR")
+    else:
+        print("INVALID CHOICE")
+        exit()
+    
+    l = ["rock","paper","scissor"]
+    computer = random.choice(l)
+    print(f"Computer choice is {computer}")
+    
+    if player == rock and computer == "rock":
+        print("it's an draw")
+    elif player == paper and computer == "paper":
+        print("it's an draw")
+    elif player == scissor and computer == "scissor":
+        print("it's an draw")
+    
+    if player == rock and computer == "scissor":
+        print("player won")
+    elif player == paper and computer == "rock":
+        print("player won")
+    elif player == scissor and computer == "paper":
+        print("player won")
 
-if player == rock:
-    print(f"YOUR CHOICE IS ROCK")
-elif player == paper:
-    print(f"YOUR CHOICE IS PAPER")
-elif player == scissor:
-    print(f"YOUR CHOICE IS SCISSOR")
-else:
-    print("INVALID CHOICE")
-    exit()
+    if player == rock and computer == "paper":
+        print("player lost")
+    elif player == paper and computer == "scissor":
+        print("player lost")
+    elif player == scissor and computer == "rock":
+        print("player lost")
 
-l = ["rock","paper","scissor"]
-computer = random.choice(l)
-print(f"Computer choice is {computer}")
-
-if player == rock and computer == "rock":
-    print("it's an draw")
-elif player == paper and computer == "paper":
-    print("it's an draw")
-elif player == scissor and computer == "scissor":
-    print("it's an draw")
-
-if player == rock and computer == "scissor":
-    print("player won")
-elif player == paper and computer == "rock":
-    print("player won")
-elif player == scissor and computer == "paper":
-    print("player won")
-
-if player == rock and computer == "paper":
-    print("player lost")
-elif player == paper and computer == "scissor":
-    print("player lost")
-elif player == scissor and computer == "rock":
-    print("player lost")
